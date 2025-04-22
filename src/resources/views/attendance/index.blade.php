43751dc76d78832afb0cc326186717b6b888e030
butterfly <serenitaey39@gmail.com>
2025-04-23 01:30:31 +0900

    日時の表示

--- src/resources/views/attendance/index.blade.php
+++ src/resources/views/attendance/index.blade.php
@@ -6,6 +6,18 @@
 
 @section('content')
 <div class="attendance">
-    <h2 class="attendance__heading">勤怠管理</h2>
+    <div class="attendance__content">
+        <div class="attendance__info">
+            @php
+                $now = \Carbon\Carbon::now();
+                $weekdays = ['日', '月', '火', '水', '木', '金', '土'];
+            @endphp
+            <div class="attendance__date-group">
+                <p class="attendance__date">{{ $now->format('Y年n月j日') }}</p>
+                <p class="attendance__day">({{ $weekdays[$now->dayOfWeek] }})</p>
+            </div>
+            <p class="attendance__time">{{ $now->format('H:i') }}</p>
+        </div>
+    </div>
 </div>
 @endsection
